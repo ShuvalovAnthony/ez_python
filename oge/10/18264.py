d23150005d77e58f2641cc7205569a95706d8df3
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2025-02-02 22:39:21 +0700

    123

---
+++ oge/10/18264.py
@@ -0,0 +1,8 @@
+print(oct(86)[2:])
+print(oct(99)[2:])
+print(oct(105)[2:])
+
+
+print(sum([int(i) for i in oct(99)[2:]]))
+print(sum([int(i) for i in oct(86)[2:]]))
+print(sum([int(i) for i in oct(105)[2:]]))
---
+++ oge/10/18294.py
@@ -0,0 +1,10 @@
+# считаем единички глазами
+print(bin(100)[2:])
+print(bin(90)[2:])
+print(bin(80)[2:])
+
+
+
+print(bin(100)[2:].count('1'))
+print(bin(90)[2:].count('1'))
+print(bin(80)[2:].count('1'))
