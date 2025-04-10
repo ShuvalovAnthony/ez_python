b3ec6c66b3a97fc6df41f1adc0c217849458ada2
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2025-04-10 16:09:18 +0700

    upd

---
+++ Daniil/24/5677/5677.py
@@ -0,0 +1,30 @@
+from string import ascii_uppercase
+
+f = open("Daniil/24/5677/24_5677.txt").read()
+
+starts = ['AD', 'D']
+endings = ['DA', 'D']
+
+# f = f.replace("DAD", '_')
+
+# for i in ascii_uppercase:
+#     f = f.replace(i, ' ')
+
+# print(max([len(i) for i in f.split()]))
+
+#   DADDADDADDADDADDADDAD   
+# AD                      DA
+# D                       D
+
+max_len = 0
+
+for i in range(50, 0, -1):
+    for start in starts:
+        for end in endings:
+            res = start + "DAD"*i + end
+            if res in f:
+                max_len = max(max_len, len(res))
+
+print(max_len)
+
+print("DAD"*32)
