b1e370a63efba1022493d97afa715e68715b63a4
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2025-06-04 20:05:30 +0700

    upd

---
+++ Nikita/lesson3.py
@@ -0,0 +1,26 @@
+from random import randint
+from string import digits
+
+
+pin_code = ""
+for i in range(4):
+    pin_code += digits[randint(0, 9)]
+
+print(pin_code) # comment later when finished code
+
+# Человек подбирает пароль
+# Если он угадал какую-то цифру, сказать что цифра номер N угадана
+# Делать так, пока не будут угаданы все 4 цифры
+
+while True:
+    print()
+    guess = input("Input pin code ")
+
+    if pin_code == guess:
+        print("Right pin")
+        break
+
+    
+    if pin_code[0] == guess[0]:
+        print("...")
+    
