678a60a5d2837ac2aa1014ef83eb029be889afdc
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2025-10-03 22:50:24 +0700

    upd

---
+++ Katia/26/20815/20815.py
@@ -0,0 +1,63 @@
+f = open("Katia/26/20815/26_20815.txt")
+
+
+data = []
+
+n, k = [int(i) for i in f.readline().split()]
+
+# print(n, k)
+
+for row in f:
+    row = [int(i) for i in row.split()]
+    id_ = row[0]
+    row = row[1:]
+
+    data.append([id_, sum(row), row[-1]])
+
+
+data = sorted(data, key=lambda x: [
+    -x[1],
+    -x[2],
+    x[0]
+])
+
+passed = data[:k]
+failed = data[k:]
+
+
+# for row in passed:
+#     print(row)
+
+# print("-----")
+
+# for row in failed[:5]:
+#     print(row)
+
+
+if passed[-1][1] == failed[0][1]:
+    poluprohod = passed[-1][1]
+else:
+    poluprohod = 0
+
+
+id_last_prohod = 0
+cand_with_poluprohod = 0
+
+
+if poluprohod:
+    # первое число в ответе ищем
+    for astro in passed:
+        if astro[1] != poluprohod:
+            id_last_prohod = astro[0]
+    # второе число
+    for astro in passed:
+        if astro[1] == poluprohod:
+            cand_with_poluprohod += 1
+    for astro in failed:
+        if astro[1] == poluprohod:
+            cand_with_poluprohod += 1
+else:
+    id_last_prohod = passed[-1][0]
+
+
+print(id_last_prohod, cand_with_poluprohod)
