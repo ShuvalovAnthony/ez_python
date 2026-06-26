1779aa0a335dcac17739a19515415a43e1279d50
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2026-06-27 01:39:07 +0700

    upd

---
+++ Alisa/HW/1.py
@@ -0,0 +1,8 @@
+# Дан текст:
+
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vulputate est vitae purus consequat commodo."
+
+# При помощи строковых методов
+# 1) Напечатать текст без знаков препинания 
+# 2) Посчитать количество букв L
+# 3) Найти индекс первой буквы i
