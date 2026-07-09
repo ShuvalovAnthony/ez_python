a154acfa2ce6f067f17459239aecbab811b13ef1
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2026-07-10 02:26:46 +0700

    upd

--- Alisa/HW/3.py
+++ Alisa/HW/3.py
@@ -1,31 +1,39 @@
 # дано число - посчитать количество цифр, с числовым значением больше 4 в
 # 16ти ричной системе счисления 
 
-# num = 3**32 + 7*3**15 - 8**14
-
-
+# 0123456789 ABCDEF
 
 
 # fruits = []
 
 # while True:
-    # fruit = input("Введите фрукт: ")
-    # дописать код, который будет добавлять в список только те фрукты,
-    # которых там еще нет
-    # если введут 0 - остановить цикл
+#     fruit = input("Введите фрукт: ")
+    
+#     if fruit == '0':
+#         break
+
+#     if fruit not in fruits:
+#         fruits.append(fruit)
+
+# print(fruits)
 
 
-
+# for i in range(10):
+#     for j in range(10):
+#         print('i=', i, 'j=', j)
 
 
 # написать цикл, который напечатает все числа, кратные 123456
 # в промежутке от 10^10 до 2*10^10
 
+# cmd+c
+# for num in range(10000059456, 2*10**10 + 1, 123456):
+#     print(num)
+    
 
 
 
-
-# num = 789236
+num = 789236
 # num_hard = 9346573451234234
 
 # дано число, необходимо найти:
@@ -35,12 +43,47 @@
 # задача посложнее - все тоже самое, с числом num_hard
 
 
+# sum_cifr = 0
+
+# for digit in str(num):
+#     sum_cifr += int(digit)
+
+# print(sum_cifr)
+
+# deliteli = set()
+
+# for i in range(1, int(num**0.5) + 1):
+#     if num%i == 0:
+#         deliteli.add(i)
+#         deliteli.add(num//i)
+
+# print(deliteli)
+
+
+# def isPrime(num):
+#     for i in range(2, int(num**0.5) + 1):
+#         if num%i == 0:
+#             return False
+#     return True
+
+
+# primeMnozh = set()
+
+# for i in range(1, int(num**0.5) + 1):
+#     if num%i == 0:
+#         if isPrime(i)
+#         primeMnozh.add(i)
+#         primeMnozh.add(num//i)
+
+
+
+
 
 # на вход будет подаваться 6 натуральных чисел через пробел
 # они сохраняются в список nums (заранее неизвестны - я буду вводить любые
 # ты для теста тоже можешь по 6 любых чисел вводить)
 
-# nums = [int(i) for i in input().split()] 
+nums = [int(i) for i in input().split()] 
 
 # print(nums)
 
@@ -50,3 +93,39 @@
 # 1) в списке есть повторяющиеся числа
 # 2) в списке количество четных и количество нечетных чисел равно
 # 3) в списке ровно 3 двузначных числа
+
+# для пункта 2
+
+def check2(nums):
+    chet = 0
+    nechet = 0
+
+    for num in nums:
+        if num%2 == 0:
+            chet += 1
+        else:
+            nechet += 1
+    
+    return chet == nechet
+
+
+# для пункта 3
+
+def check3(nums):
+    kolvo_dvuznach = 0
+
+    for num in nums:
+        if 10 <= num <= 99:
+            kolvo_dvuznach += 1
+    
+    return kolvo_dvuznach == 2
+        
+
+if (
+    (len(nums) != len(set(nums))) and
+    check2(nums) and
+    check3(nums)
+):
+    print("YES")
+else:
+    print("NO")
--- Alisa/Theory/3.py
+++ Alisa/Theory/3.py
@@ -1,9 +1,107 @@
-for 
-while
+# for 
+# while
 
 
-if/elif/else
+# if/elif/else
 
 
-if num > 5:
-    print("YES")
+# if num > 5:
+#     print("YES")
+
+# 28926
+# functions
+
+
+# функции - это участок кода, который можно переиспользовать
+# множественными вызовами в том числе с разными аргументами/параметрами
+
+# функция это рецепт/инструкция к работе
+
+# def greetings(name): # def - define - СОЗДАНИЕ функции
+#     return "Hello " + name
+
+
+# вызов функции - запуск работника
+
+
+# def isPrime(num):
+#     for i in range(2, int(num**0.5) + 1):
+#         if num%i == 0:
+#             return False
+#     return True
+
+
+# for num in range(1, 1000):
+#     prostoe = True
+
+#     for i in range(2, int(num**0.5) + 1):
+#         if num%i == 0:
+#             prostoe = False
+    
+#     if prostoe:
+#         print(num, "prostoe chislo")
+
+
+
+# def to_3(n):
+#     tri_n = ''
+
+#     while n > 0:
+#         tri_n += str(n%3)
+#         n //= 3
+
+#     return tri_n[::-1]
+
+# res = []
+
+# for n in range(1, 1000):
+#     tri_n = to_3(n)
+
+#     if n%3 == 0:
+#         tri_n += tri_n[-2:]
+#     else:
+#         sum_cifr = sum([int(i) for i in tri_n])
+#         sum_cifr *= 2
+
+#         tri_n += to_3(sum_cifr)
+
+#     r = int(tri_n, 3)
+
+#     if r > 520 and r%2 == 1:
+#         res.append(r)
+
+# print(min(res))
+
+
+
+# число оканчивается на четную цифру
+
+# ==
+# != 
+# and or
+# > < >= <=
+
+# def check(num): # True/False
+#     return num%10%2 == 0
+    
+# num = 55
+
+# if check(num):
+#     print("оканчивается на четную")
+# else:
+#     print("не оканчивается на четную")
+
+
+
+# nums = [1, 2, 3, 1, 2, 3]
+
+# print(len(set(nums)))
+
+
+# функция которая говорит есть ли в тексте гласные
+# def check(text: str):
+#     for glas in "aeyuio":
+#         if glas in text.lower():
+#             return True
+
+#     return False
